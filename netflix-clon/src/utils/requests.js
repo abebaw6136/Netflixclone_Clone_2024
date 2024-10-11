d7ed4f998e070af7403c1bf0ac527f1a86da876e
abebaw1616@gmail.com <Local Admin@ETNTSRSLP061361.ethio.local>
2024-10-11 23:30:02 +0300

    Row

--- netflix-clon/src/utils/requests.js
+++ netflix-clon/src/utils/requests.js
@@ -1,9 +1,8 @@
 const API_KEY = process.env.REACT_APP_API_KEY; // Ensure this is correct
 
-
-const requests={
+const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchAdventureMovies: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
@@ -12,8 +11,9 @@ const requests={
     fetchDramaMovies: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    ffetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, // Corrected this line
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
+
 export default requests;
