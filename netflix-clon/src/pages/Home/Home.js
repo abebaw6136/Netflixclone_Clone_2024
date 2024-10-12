124d8f70ad810271f2437a1d5d9a85a6c4c09726
abebaw1616@gmail.com <Local Admin@ETNTSRSLP061361.ethio.local>
2024-10-12 17:21:31 +0300

    deploy

--- netflix-clon/src/pages/Home/Home.js
+++ netflix-clon/src/pages/Home/Home.js
@@ -1,32 +1,4 @@
-/*import React, { components } from 'react'
-import React from 'react';
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
 
-const Home = () =>{
- 
-    return (
-        <>
-
-      <Header/>
-      <Footer/>
-      </>
-    );
-  };
-
-export default Home */
-
-/*import React from 'react';
-
-const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to the Netflix Clone Home Page!</h1>
-        </div>
-    );
-};
-
-export default Home;*/ // Ensure this line is present
 
 import React from 'react';
 import Header from '../../components/Header/Header';
